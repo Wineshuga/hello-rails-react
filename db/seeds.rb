7b59169b4bb3b7fcf3f5915872e3cf3a0db7f9fd
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-29 00:02:20 +0100

    Create table for messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+greetings = [
+  "Hello, how are you?",
+  "Good morning!",
+  "Hi there!",
+  "Greetings!",
+  "Welcome!"
+]
+
+greetings.each do |greeting|
+  Message.create(content: greeting)
+end
