666b7fd870002085b2a9c2add020f7d758d2c8b4
Wineshuga <uzochukwuwinnie@gmail.com>
2023-11-29 01:29:27 +0100

    Create App component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route} from 'react-router-dom';
+import Greeting from './Greeting';
+
+const App = () => {
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </main>
+  )
+}
+export default App
